a3a1e8918aa53c24c4efa4bee934a8f760cdc114
Zach Moss <88854718+ZachMoss31@users.noreply.github.com>
2025-09-21 22:31:53 -0600

    Initial Project Commit
    Adding initial commit and setup in preparation for DX12 renderer

---
+++ Wraith3DEngine/Wraith3DEngine/Hello.cpp
@@ -0,0 +1,14 @@
+#include <iomanip>
+#include <iostream>
+
+/// <summary>
+/// Quick setup testing everything is compiling etc
+/// </summary>
+void main()
+{
+	std::cout << "Hello, Wraith3D!";
+
+	// Print versions of CUDA, C++, Compiler, DX12.
+
+	std::cin.get();
+}
